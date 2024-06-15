dde3fbb7c9ca1c2101f28123e656a1d5cc4a0e8a
rikich3 <rchambillap@unsa.edu.pe>
2024-06-15 17:26:18 -0500

    Colocando todas las primeras letras de la lista en Mayuscula usando filters e imprimiendo la hora actual con el tag now

--- djangoPPT/djangovenv/listaContactos/personas/views.py
+++ djangoPPT/djangovenv/listaContactos/personas/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def inspectPersona(request):
   myContext = {
+    'id' : id,
     'Persona': ['nombre', 'apellido', 'edad', 'donador'],
   }
   return render(request, 'persona.html', myContext)
