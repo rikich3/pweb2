5870639356b65c1c9b32603685abec3c655ea431
rikich3 <rchambillap@unsa.edu.pe>
2024-06-15 17:53:10 -0500

    independizando los templates de la aplicacion personas, creando su propio directorio y modificando el view para que sepa cual template renderizar

--- djangoPPT/djangovenv/listaContactos/personas/views.py
+++ djangoPPT/djangovenv/listaContactos/personas/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Persona
 
 # Create your views here.
 def inspectPersona(request):
+  id = request.GET.get('id', '0')
+  person = Persona.objects.get(id=id)
   myContext = {
     'id' : id,
-    'Persona': ['nombre', 'apellido', 'edad', 'donador'],
+    'Persona': person
   }
-  return render(request, 'persona.html', myContext)
+  return render(request, 'personas/persona.html', myContext)
