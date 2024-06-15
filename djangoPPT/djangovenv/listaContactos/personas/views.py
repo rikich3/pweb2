a973837a172f864a5ceb76f3d707147b67e93845
rikich3 <rchambillap@unsa.edu.pe>
2024-06-15 16:33:31 -0500

    Creando una vista a la aplicacion personas, usando un contextos, imprimimos el esqueleto del modelo Personas

--- djangoPPT/djangovenv/listaContactos/personas/views.py
+++ djangoPPT/djangovenv/listaContactos/personas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inspectPersona(request):
+  myContext = {
+    'Persona': ['nombre', 'apellido', 'edad', 'donador'],
+  }
+  return render(request, 'persona.html', myContext)
