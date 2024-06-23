125db474bc5ec8c2ac7e3042bddf31928c01f683
rikich3 <rchambillap@unsa.edu.pe>
2024-06-23 16:41:57 -0500

    Manipulando el formulario en la vista para poder crear y añadir Personas, realizando las comprobaciones de datos en el servidor

--- djangoPPT/djangovenv/listaContactos/personas/views.py
+++ djangoPPT/djangovenv/listaContactos/personas/views.py
@@ -5,6 +5,13 @@ from .personaForm import PersonaForm, RawPersonaForm
 # Create your views here.
 def createUpdatePersona(request):
   form = RawPersonaForm()
+  if(request.method == 'POST'):
+    form = RawPersonaForm(request.POST)
+    if form.is_valid():
+      print(form.cleaned_data)
+      Persona.objects.create(**form.cleaned_data)
+    else:
+      print(form.errors)
   context = {
     'form': form
   }
