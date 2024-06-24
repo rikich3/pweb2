a8d08e8e9b81ac8c0f7700cf6f51b04246f4661b
rikich3 <rchambillap@unsa.edu.pe>
2024-06-23 19:53:36 -0500

    Modificando el campos del raw form nombres de personas para que tenga un widget y añadiendo al form validacion en el campo de nombre para que no se ingrese un usuario con el nombre de admin y que su nombre comienze con una mayus

--- djangoPPT/djangovenv/listaContactos/personas/personaForm.py
+++ djangoPPT/djangovenv/listaContactos/personas/personaForm.py
@@ -11,3 +11,11 @@ class PersonaForm(forms.ModelForm):
       'donador': '¿Es donador?',
       'correo': 'Correo electrónico'
     }
+  def clean_nombre(self, *args, **kwargs):
+    name = self.cleaned_data.get('nombre')
+    if name == 'admin':
+      raise forms.ValidationError('Invalid name')
+    if not name.istitle():
+      raise forms.ValidationError('Name must start with a capital letter')
+    return name
+    
--- djangoPPT/djangovenv/listaContactos/personas/rawform.py
+++ djangoPPT/djangovenv/listaContactos/personas/rawform.py
@@ -1,7 +1,15 @@
 import django.forms as forms
 
 class RawPersonaForm(forms.Form):
-  nombres = forms.CharField()
+  nombres = forms.CharField(
+    widget=forms.Textarea(
+      attrs={
+        'placeholder': 'Your name',
+        'class': 'form-control',
+        'id': 'nombres'
+      }
+    )
+  )
   apellidos = forms.CharField()
   donador = forms.BooleanField()
   edad = forms.IntegerField()
